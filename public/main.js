89a16709180ce66d4ada51d3172d475a49f64bcc
hohoto <540172574@qq.com>
2020-09-19 20:43:04 +0800

    add page

--- public/main.js
+++ public/main.js
@@ -1,23 +1,23 @@
 getCSS.onclick = () => {
-    const request = new XMLHttpRequest();
-    request.open("GET", "/style.css"); 
-    request.onreadystatechange = () => {
-      if (request.readyState === 4) {
-        if (request.status >= 200 && request.status < 300) {
-          const style = document.createElement("style");
-          style.innerHTML = request.response;
-          document.head.appendChild(style);
-        } else {
-          alert("加载 CSS 失败");
-        }
+  const request = new XMLHttpRequest();
+  request.open("GET", "/style.css");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      } else {
+        alert("加载 CSS 失败");
       }
-    };
-    request.send(); // readyState = 2
+    }
+  };
+  request.send(); // readyState = 2
 };
-  
+
 getJS.onclick = () => {
   const request = new XMLHttpRequest();
-  request.open("GET", "/2.js"); 
+  request.open("GET", "/2.js");
   request.onreadystatechange = () => {
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
@@ -34,7 +34,7 @@ getJS.onclick = () => {
 
 getHTML.onclick = () => {
   const request = new XMLHttpRequest();
-  request.open("GET", "/3.html"); 
+  request.open("GET", "/3.html");
   request.onreadystatechange = () => {
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
@@ -51,7 +51,7 @@ getHTML.onclick = () => {
 
 getXML.onclick = () => {
   const request = new XMLHttpRequest();
-  request.open("GET", "/4.xml"); 
+  request.open("GET", "/4.xml");
   request.onreadystatechange = () => {
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
@@ -68,7 +68,7 @@ getXML.onclick = () => {
 
 getJSON.onclick = () => {
   const request = new XMLHttpRequest();
-  request.open("GET", "/5.json"); 
+  request.open("GET", "/5.json");
   request.onreadystatechange = () => {
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
@@ -80,4 +80,22 @@ getJSON.onclick = () => {
     }
   };
   request.send(); // readyState = 2
+};
+
+let n = 1;
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n + 1}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const array = JSON.parse(request.response);
+      array.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item.id;
+        xxx.appendChild(li);
+      });
+      n += 1
+    }
+  };
+  request.send();
 };
